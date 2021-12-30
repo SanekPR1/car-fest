122b1dd689cb5f6c7a9c004e65e2f873ae7b1c4b
Aleksandr Makridin <sanekpr1@gmail.com>
2021-12-30 21:10:18 +0100

    #034: added routers to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { SpaModule } from 'src/spa/spa.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHomeComponent } from './routes/app-home/app-home.component';
+import { appRoutes } from './routes/app.routes';
 import { CarsComponent } from './routes/cars/cars.component';
 import { SettingsComponent } from './routes/settings/settings.component';
 
@@ -17,8 +19,8 @@ import { SettingsComponent } from './routes/settings/settings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SpaModule
+    SpaModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
