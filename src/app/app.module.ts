e6d3e0cf8c34958c9a766ca1aee00864edf1e26a
Aleksandr Makridin <sanekpr1@gmail.com>
2021-12-30 21:06:34 +0100

    #033: moved home, cars and setting to the route package + changed imports in app module + added router file with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { SpaModule } from 'src/spa/spa.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppHomeComponent } from './app-home/app-home.component';
-import { CarsComponent } from './cars/cars.component';
-import { SettingsComponent } from './settings/settings.component';
+import { AppHomeComponent } from './routes/app-home/app-home.component';
+import { CarsComponent } from './routes/cars/cars.component';
+import { SettingsComponent } from './routes/settings/settings.component';
 
 @NgModule({
   declarations: [
