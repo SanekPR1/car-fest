9b89d7884b6a7801d421f946b5779ad253ade48c
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-02 20:35:11 +0100

    #056: made email as a username

--- src/spa/users/users-api.ts
+++ src/spa/users/users-api.ts
@@ -2,6 +2,6 @@ import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
 import { Observable } from "rxjs";
 
 export abstract class UserApi {
-    signIn: (username: StringMap, password: string) => Observable<any>;
+    signIn: (email: string, password: string) => Observable<any>;
     signOut: () => Observable<any>;
 } 
