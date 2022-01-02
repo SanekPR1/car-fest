2d2da9dae1ed823d0eb13630a5a9cd50246774d0
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-02 21:13:45 +0100

    #061: made hidden all the pages under authenticated

--- src/app/app.menu.ts
+++ src/app/app.menu.ts
@@ -2,25 +2,25 @@ import { MenuItem } from "src/spa/services/menu.service";
 
 export const AppMenuItems: Array<MenuItem> = [
     {
-        text: 'Cars', icon: 'imgs/car.png', route: '/cars', subMenu: [
+        text: 'Cars', icon: 'imgs/car.png', route: null, subMenu: [
             {
                 text: 'Select', icon: 'imgs/car.png', route: null, subMenu: [
-                    { text: 'Audi', icon: 'imgs/car.png', route: '/car-detail/audi', subMenu: null },
-                    { text: 'BMW', icon: 'imgs/car.png', route: '/car-detail/bmw', subMenu: null },
-                    { text: 'Fiat', icon: 'imgs/car.png', route: '/car-detail/fiat', subMenu: null },
-                    { text: 'Ford', icon: 'imgs/car.png', route: '/car-detail/ford', subMenu: null }
+                    { text: 'Audi', icon: 'imgs/car.png', route: '/authenticated/car-detail/audi', subMenu: null },
+                    { text: 'BMW', icon: 'imgs/car.png', route: '/authenticated/car-detail/bmw', subMenu: null },
+                    { text: 'Fiat', icon: 'imgs/car.png', route: '/authenticated/car-detail/fiat', subMenu: null },
+                    { text: 'Ford', icon: 'imgs/car.png', route: '/authenticated/car-detail/ford', subMenu: null }
                 ]
             },
-            { text: 'Pric top', icon: 'imgs/car.png', route: '/car-list/1', subMenu: null },
-            { text: 'Top 3', icon: 'imgs/car.png', route: '/car-list/3', subMenu: null },
-            { text: 'Top 5', icon: 'imgs/car.png', route: '/car-list/5', subMenu: null }
+            { text: 'Pric top', icon: 'imgs/car.png', route: '/authenticated/car-list/1', subMenu: null },
+            { text: 'Top 3', icon: 'imgs/car.png', route: '/authenticated/car-list/3', subMenu: null },
+            { text: 'Top 5', icon: 'imgs/car.png', route: '/authenticated/car-list/5', subMenu: null }
         ]
     },
     {
         text: 'Maintenance', icon: 'imgs/settings.png', route: null, subMenu: [
-            { text: 'Car maintenance', icon: 'imgs/settings.png', route: '/car-maint', subMenu: null },
-            { text: 'Settings', icon: 'imgs/settings.png', route: '/settings', subMenu: null }
+            { text: 'Car maintenance', icon: 'imgs/settings.png', route: '/authenticated/car-maint', subMenu: null },
+            { text: 'Settings', icon: 'imgs/settings.png', route: '/authenticated/settings', subMenu: null }
         ]
     },
-    { text: 'Home', icon: 'imgs/home.png', route: '/home', subMenu: null }
+    { text: 'Home', icon: 'imgs/home.png', route: '/authenticated/home', subMenu: null }
 ] 
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { RegistrationComponent } from "src/spa/users/registration/registration.component";
 import { SignInComponent } from "src/spa/users/sign-in/sign-in.component";
+import { AuthGuardService } from "../services/auth-guard.service";
 import { AppHomeComponent } from "./app-home/app-home.component";
 import { AuthenticatedComponent } from "./authenticated/authenticated.component";
 import { CarDetailComponent } from "./car-detail/car-detail.component";
@@ -12,12 +13,16 @@ export const appRoutes: Routes = [
     { path: 'sign-in', component: SignInComponent },
     { path: 'register', component: RegistrationComponent },
     {
-        path: 'authenticated', component: AuthenticatedComponent, children: [
-            { path: 'home', component: AppHomeComponent },
-            { path: 'settings', component: SettingsComponent },
-            { path: 'car-list/:count', component: CarListComponent },
-            { path: 'car-detail/:car', component: CarDetailComponent },
-            { path: 'car-maint', component: CarMaintComponent }
+        path: 'authenticated', component: AuthenticatedComponent, canActivate: [AuthGuardService], children: [
+            {
+                path: '', canActivateChild: [AuthGuardService], children: [
+                    { path: 'home', component: AppHomeComponent },
+                    { path: 'settings', component: SettingsComponent },
+                    { path: 'car-list/:count', component: CarListComponent },
+                    { path: 'car-detail/:car', component: CarDetailComponent },
+                    { path: 'car-maint', component: CarMaintComponent }
+                ]
+            },
         ]
     },
     { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
