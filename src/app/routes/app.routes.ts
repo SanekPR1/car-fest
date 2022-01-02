d8b968767f8019cebd18fd7703b9a3aeba96bf5b
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-02 13:01:13 +0100

    #050: changed routers dependency to new authenticated component + changed app.component.html layout

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,16 +1,21 @@
 import { Routes } from "@angular/router";
 import { AppHomeComponent } from "./app-home/app-home.component";
+import { AuthenticatedComponent } from "./authenticated/authenticated.component";
 import { CarDetailComponent } from "./car-detail/car-detail.component";
 import { CarListComponent } from "./car-list/car-list.component";
 import { CarMaintComponent } from "./car-maint/car-maint.component";
 import { SettingsComponent } from "./settings/settings.component";
 
 export const appRoutes: Routes = [
-    { path: 'home', component: AppHomeComponent },
-    { path: 'settings', component: SettingsComponent },
-    { path: 'car-list/:count', component: CarListComponent },
-    { path: 'car-detail/:car', component: CarDetailComponent },
-    { path: 'car-maint', component: CarMaintComponent },
+    {
+        path: 'authenticated', component: AuthenticatedComponent, children: [
+            { path: 'home', component: AppHomeComponent },
+            { path: 'settings', component: SettingsComponent },
+            { path: 'car-list/:count', component: CarListComponent },
+            { path: 'car-detail/:car', component: CarDetailComponent },
+            { path: 'car-maint', component: CarMaintComponent }
+        ]
+    },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', component: AppHomeComponent } // if incorrect route 
 ]
