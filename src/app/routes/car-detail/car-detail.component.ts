a5c3bc5ba73cd432318ce7ab53ba6bcfc98d4199
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-03 19:30:43 +0100

    #066:created field interface with fields definition for Car for dynamic forms + added these definitions into car-detail.component + defined onInit method logic

--- src/app/routes/car-detail/car-detail.component.ts
+++ src/app/routes/car-detail/car-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldInput } from 'src/spa/dynamic-forms/field-interface';
+import { Car } from 'src/app/services/car-interface';
+import { AppDataService } from 'src/app/services/app-data.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-car-detail',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-detail.component.css']
 })
 export class CarDetailComponent implements OnInit {
+  car: Car;
+  carDefinitionInput: Array<FieldInput> = [
+    { key: 'id', type: 'number', isId: true, label: 'Id', required: true },
+    { key: 'name', type: 'string', isId: false, label: 'Car name', required: true },
+    { key: 'model', type: 'string', isId: false, label: 'Car model', required: true },
+    { key: 'price', type: 'number', isId: false, label: 'Car price value', required: true },
+  ];
+  operation: string; //edit; read; create
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataService: AppDataService
+  ) { }
 
   ngOnInit(): void {
+    this.operation = this.route.snapshot.params['operation'];
+    if (this.operation === 'create') {
+      this.car = { id: 0, name: '', model: '', price: null }
+    } else {
+      this.dataService.getCar(+this.route.snapshot.params['id'])
+        .subscribe((car: Car) => {
+          this.car = car;
+        });
+    }
   }
 
 }
