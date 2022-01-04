c4b318efe8fa8a2ed4da6369b0057b9fd7e5b641
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-04 11:42:48 +0100

    #067:created dynamic-field.component

---
+++ src/spa/dynamic-forms/dynamic-field/dynamic-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spa-dynamic-field',
+  templateUrl: './dynamic-field.component.html',
+  styleUrls: ['./dynamic-field.component.css']
+})
+export class DynamicFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
