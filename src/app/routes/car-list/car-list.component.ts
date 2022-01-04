fcfaba42cac5c2f6f61669a49c59567667283e51
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-04 21:21:18 +0100

    #076: added logic to car-list.component + added spa-panel & car-panel into car-list's layout

--- src/app/routes/car-list/car-list.component.ts
+++ src/app/routes/car-list/car-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { AppDataService } from 'src/app/services/app-data.service';
+import { Car } from 'src/app/services/car-interface';
 
 @Component({
   selector: 'app-car-list',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-list.component.css']
 })
 export class CarListComponent implements OnInit {
+  allCars: Array<Car>;
+  cars: Array<Car>;
+  count = 0;
 
-  constructor() { }
+  constructor(private dataService: AppDataService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.dataService.getCars().subscribe(items => {
+      this.allCars = items;
+      this.count = +this.route.snapshot.params['count'];
+      this.updateList();
+      this.route.params.forEach((params: Params) => {
+        this.count = +params['count'];
+        this.updateList();
+      })
+    })
   }
 
+  updateList() {
+    const AllCarsCopy = this.allCars.slice().sort((a, b) => this.compareSort(a, b));
+    this.cars = (this.count > 0) ? AllCarsCopy.slice(0, this.count) : this.allCars;
+  }
+
+  compareSort(carA: Car, carB: Car): number {
+    return carB.price - carA.price;
+  }
 }
