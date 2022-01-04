b0c63e9ee6fdae9f40472b127d92c9108a0dc802
Aleksandr Makridin <sanekpr1@gmail.com>
2022-01-04 12:38:43 +0100

    #069: added Input fields to dynamic-field.component + added layout for fields

--- src/spa/dynamic-forms/dynamic-field/dynamic-field.component.ts
+++ src/spa/dynamic-forms/dynamic-field/dynamic-field.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FieldInput } from '../field-interface';
 
 @Component({
   selector: 'spa-dynamic-field',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamic-field.component.css']
 })
 export class DynamicFieldComponent implements OnInit {
+  @Input() field: FieldInput;
+  @Input() form: FormGroup;
+  @Input() operation: string;
+  @Input() submitted: boolean;
+
+  get isValid() {
+    return this.form.controls[this.field.key].valid;
+  }
 
   constructor() { }
 
